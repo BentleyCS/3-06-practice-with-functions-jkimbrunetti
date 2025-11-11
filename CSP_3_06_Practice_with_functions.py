22e914960aef4692754cc78aedac9eefdfd3092b
jkimbrunetti <jkim-brunetti27@bentleyschool.org>
2025-11-10 22:35:19 -0800

    Add files via upload

--- CSP_3_06_Practice_with_functions.py
+++ CSP_3_06_Practice_with_functions.py
@@ -1,44 +1,31 @@
 #Herons Formula
 import math
 
-#returns the square root of the number n
 def root(n):
     return math.sqrt(n)
 
-#Takes in the 3 side lengths of a triangle as arguments and returns half of
-#the perimeter of a triangle.
-def semiPerimeter():
-    pass
+def semiPerimeter(x,y,z):
+    return (x+y+z)/2
 
-#Modify the below function such that it takes in 4 arguments. multiply the first
-#argument by the difference between itself and each individual argument. Reference herons formula for more context.
-def multiplyDifferences():
-    pass
+def multiplyDifferences(a,b,c,d):
+    return a*(a-b)*(a-c)*(a-d)
 
-#Given the 3 sides of a triangle return the area.
-#use herons formula
-#Use the functions above.
-def herons():
-    pass
+def herons(x,y,z):
+    s = semiPerimeter(x,y,z)
+    return root(multiplyDifferences(s,x,y,z))
 
+def denominator(n):
+    return n*2
 
-#quadratic equation
+def plusMinus(x,y):
+    plus = (x*-1)+y
+    minus = (x*-1)-y
+    return plus,minus
 
-#takes in a number as an argument and returns that number multiplied by 2.
-def denominator():
-    pass
-
-#Takes in two arguments. multiply the first argument by negative 1. Then
-#return the modified first argument added and subtracted by the second argument.
-def plusMinus():
-    pass
-#takes in three numbers as arguments. The first and third multiplied together then
-#multiplied by 4.Then subtract that result from the second argument squared.
-#Return the overall result.
-def mainCalc():
-    pass
+def mainCalc(x,y,z):
+    return (y*y)-(x*z*4)
 
 #The below function should take the inputs of the quadratic equation and return the result
 #Make sure to use all the formulas from this section.
-def quadratic():
-    pass
+def quadratic(x,y,z):
+    return plusMinus(y,root(mainCalc(x,y,z)))[0]/denominator(x), plusMinus(y,root(mainCalc(x,y,z)))[1]/denominator(x)
